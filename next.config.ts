767c814536658649b8d9350a92c2b2030e3d4846
jerson gil <j.gil@goatfinance.io>
2025-08-07 09:01:02 -0400

    feat: add environment variables for Supabase and configure Next.js for production

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/personal_finance' : '',
+  assetPrefix: isProd ? '/personal_finance/' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
