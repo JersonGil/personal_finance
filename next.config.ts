19a50d1bd5324d8cd39698b81b0d52849614299b
jerson gil <j.gil@goatfinance.io>
2025-08-08 20:29:19 -0400

    fix: remove unused production configurations from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  basePath: isProd ? '/personal_finance' : '',
-  assetPrefix: isProd ? '/personal_finance' : ''
+  reactStrictMode: true
 };
 
 export default nextConfig;
